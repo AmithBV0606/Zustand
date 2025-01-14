e14ef39213d2f00f4e6ce804db8d819d3bac1bd2
AmithBV0606 <amithrao0606@gmail.com>
2025-01-14 11:16:18 +0530

    Created a feature to add the habbit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Box, Container, Typography } from "@mui/material";
 import useHabitStore from "./store/store";
+import AddHabitForm from "./components/add-habit-form";
 
 const App = () => {
   const store = useHabitStore();
   console.log(store);
 
-  return <div>App</div>;
+  return (
+    <Container>
+      <Box>
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Habit Tracker
+        </Typography>
+
+        {/* Form */}
+        <AddHabitForm />
+
+        {/* List */}
+        {/* Stats */}
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
